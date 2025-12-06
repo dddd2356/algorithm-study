6d7afdeb310bc855da8952f278ae8423608152b0
dddd2356 <dddd2356@naver.com>
2025-12-06 14:17:25 +0900

    2025.12.06

---
+++ src/baekjoon/Problem2745.java
@@ -0,0 +1,29 @@
+package baekjoon;
+
+import java.util.Scanner;
+
+public class Problem2745 {
+    public static void main(String[] args){
+        Scanner sc = new Scanner(System.in);
+        String N = sc.next();
+        int B = sc.nextInt();
+
+        long result = 0L;
+        for (int i = 0; i < N.length(); i++) {
+            char c = N.charAt(i);
+            int value;
+            if (Character.isDigit(c)) {
+                value = c - '0';
+            } else {
+                value = Character.toUpperCase(c) - 'A' + 10;
+            }
+            if (value >= B) {
+                throw new IllegalArgumentException("Digit '" + c + "' is not valid for base " + B);
+            }
+            result = result * B + value;
+        }
+
+        System.out.println(result);
+        sc.close();
+    }
+}
