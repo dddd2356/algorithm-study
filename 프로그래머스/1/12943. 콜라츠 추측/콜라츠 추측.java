5f8003d2097a4711fb41d52b08903920ec4d1252
dddd2356 <140783383+dddd2356@users.noreply.github.com>
2026-06-08 12:37:32 +0900

    [level 1] Title: 콜라츠 추측, Time: 0.05 ms, Memory: 75.9 MB -BaekjoonHub

---
+++ 프로그래머스/1/12943. 콜라츠 추측/콜라츠 추측.java
@@ -0,0 +1,27 @@
+class Solution {
+    public int solution(int num) {
+        if (num == 1) return 0;
+        
+        int answer = 0;
+        int count = 0;
+        long n = num;
+        
+        while (n != 1 && count < 500) {
+            if (n % 2 == 0) {
+                n = n / 2;
+            } 
+            else {
+                n = (n * 3) + 1;
+            }
+            count++;
+        }
+        
+        if(count>=500){
+            answer = -1;
+        }
+        else
+            answer = count;
+        
+        return answer;
+    }
+}
