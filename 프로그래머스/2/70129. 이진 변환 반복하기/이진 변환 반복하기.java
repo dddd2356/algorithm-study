ad8e3d293630e1e1ba93600a4cf750ad6595f0e6
dddd2356 <140783383+dddd2356@users.noreply.github.com>
2026-06-08 10:41:27 +0900

    [level 2] Title: 이진 변환 반복하기, Time: 11.09 ms, Memory: 91.5 MB -BaekjoonHub

---
+++ 프로그래머스/2/70129. 이진 변환 반복하기/이진 변환 반복하기.java
@@ -0,0 +1,20 @@
+class Solution {
+    public int[] solution(String s) {
+        int transformCount = 0;
+        int zeroCount = 0;
+
+        while (!s.equals("1")) {
+            int originalLength = s.length();
+        
+            s = s.replaceAll("0", "");
+            int newLength = s.length();
+            
+            zeroCount += (originalLength - newLength);
+            
+            s = Integer.toBinaryString(newLength);
+            transformCount++;
+        }
+
+        return new int[]{transformCount, zeroCount};
+    }
+}
